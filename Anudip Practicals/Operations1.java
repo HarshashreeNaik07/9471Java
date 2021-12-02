651d7a5db4749f336ceb7bfa150733b0fbe5e4ea
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-02 10:35:16 +0530

    Create Operations1.java

---
+++ Anudip Practicals/Operations1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Operations1{
+public static void main(String args[]){
+Scanner in = new Scanner (System.in);
+System.out.println("Enter first number:");
+int num1 = in.nextInt();
+System.out.println("Enter second number");
+int num2 = in.nextInt();
+System.out.println("Addition: " +(num1+num2));
+System.out.println("Subtraction: " +(num1-num2));
+System.out.println("Multiplication: " +(num1*num2));
+System.out.println("Division: " +(num1/num2));
+}}
