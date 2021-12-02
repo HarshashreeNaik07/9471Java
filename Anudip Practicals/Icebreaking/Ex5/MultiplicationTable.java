893ba057c8f9838cdb26eaab2e593032daad242a
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-02 21:20:07 +0530

    Update and rename Anudip Practicals/Icebreaking/MultiplicationTable.java to Anudip Practicals/Icebreaking/Ex5/MultiplicationTable.java

--- Anudip Practicals/Icebreaking/Ex5/MultiplicationTable.java
+++ Anudip Practicals/Icebreaking/Ex5/MultiplicationTable.java
@@ -1,3 +1,6 @@
+//Write a Java program that takes a number as input and prints its multiplication table upto 10. Go to the editor
+//Test Data:
+//Input a number: 8
 import java.util.Scanner;
 public class MultiplicationTable{
 public static void main(String args[]){
