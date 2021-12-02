f7e5b15afea5a6795b937c69ebb76564ea9ae245
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-02 22:11:35 +0530

    Create RelationalOperator.java

---
+++ Anudip Practicals/RelationalOperator.java
@@ -0,0 +1,23 @@
+public class RelationalOperator
+{
+	public static void main(String args[])
+	{
+	 int a = 50;
+	 int b = 50;
+	 if(a==b)
+	   {
+	 System.out.println("a and b are equal");
+	   }
+	 else{
+	 System.out.println("a and b are not equal");
+	 }
+
+	  if(a!=b)
+    {
+	   System.out.println("a and b are not equal");
+	  }
+	  else{
+	  System.out.println("a and b are equal");
+	  }
+	}
+}
