1284630bd93430461e346fe1fbf43b02da87e1d5
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-02 21:21:24 +0530

    Update and rename Anudip Practicals/Icebreaking/PositiveorNegative1.java to Anudip Practicals/Icebreaking/Ex3/PositiveorNegative1.java

--- Anudip Practicals/Icebreaking/Ex3/PositiveorNegative1.java
+++ Anudip Practicals/Icebreaking/Ex3/PositiveorNegative1.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
+//Write a Java program to get a number from the user and print whether it is positive or negative.
 
+import java.util.Scanner;
 public class PositiveorNegative1{
 public static void main(String args[]){ 
 int a;
