373ff1bfce600cc9ae8282c8a8257e7e5fa12f8f
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-02 22:07:15 +0530

    Create Pyramid2.java

---
+++ Anudip Practicals/Pyramid2.java
@@ -0,0 +1,10 @@
+//for loop pattern pyramid
+
+public class Pyramid2{
+	public static void main(String args[]){
+	for(int i=0;i<=6;i++){
+	 for(int j=6;j>=i;j--){
+	  System.out.print("*");
+	  }
+	 System.out.println();
+	 }}}
