e6cd3e1774633deea31349d3fee32aeb6849833a
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-02 20:55:04 +0530

    Create MInheritance.java

---
+++ Anudip Practicals/MInheritance.java
@@ -0,0 +1,23 @@
+class Animal{
+void eat(){
+System.out.println("Eating");}
+}
+
+class Dog extends Animal{
+void bark(){
+System.out.println("Barking");}
+}
+
+class Puppy extends Dog{
+void weep(){
+System.out.println("weeping");}
+}
+
+class MInheritance{
+public static void main(String args[]){
+Puppy p = new Puppy();
+p.bark();
+p.weep();
+p.eat();
+}
+}
