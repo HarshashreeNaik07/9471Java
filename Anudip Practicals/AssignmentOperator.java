9e60b8b7e67960b1f5177f0f25a60ef69a8db9b8
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-02 21:42:00 +0530

    Create AssignmentOperator.java

---
+++ Anudip Practicals/AssignmentOperator.java
@@ -0,0 +1,20 @@
+public class AssignmentOperator
+{
+	public static void main(String args[])
+	{
+	  int num1=100;
+	  int num2=20;
+	  num2=num1;
+	  System.out.println("Output: "+num2);
+	  num2+=num1;
+	  System.out.println("+=Output: "+num2);
+	  num2-=num1;
+	  System.out.println("-=Output: "+num2);
+	  num2*=num1;
+	  System.out.println("*=Output: "+num2);
+	  num2/=num1;
+ 	  System.out.println("/=Output: "+num2);
+	  num2%=num1;
+	  System.out.println("%=Output: "+num2);
+	}
+}
