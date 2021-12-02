8a3dd167a62622b007d4a32927a6ae346ac7ca67
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-02 21:44:33 +0530

    Create EvenorOdd.java

---
+++ Anudip Practicals/EvenorOdd.java
@@ -0,0 +1,11 @@
+public class EvenorOdd{
+	public static void main(String args[]){
+	 int a = 65;
+	 if(a%2==0){
+	  System.out.println("Number is Even");
+	  }
+	  else{
+	  System.out.println("Number is Odd");
+	  }
+	}
+}
