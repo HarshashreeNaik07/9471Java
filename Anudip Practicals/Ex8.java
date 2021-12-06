751fe3f7faef00d0b4623e6bde6fcaf69c4a5d1a
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-06 14:39:30 +0530

    Update Ex8.java

--- Anudip Practicals/Ex8.java
+++ Anudip Practicals/Ex8.java
@@ -1,3 +1,9 @@
+//Write a Java program to display the following pattern.
+// J    a   v     v  a                                                  
+//   J   a a   v   v  a a                                                 
+//J  J  aaaaa   V V  aaaaa                                                
+// JJ  a     a   V  a     a
+
 public class Ex8
 {
 public static void main(String args[])
