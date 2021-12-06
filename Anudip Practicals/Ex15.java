13a91ebf898652028517576ead58b7af3b2923b0
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-06 14:36:39 +0530

    Create Ex15.java

---
+++ Anudip Practicals/Ex15.java
@@ -0,0 +1,16 @@
+//Write a Java program to swap two variables
+
+public class Ex15
+{
+public static void main(String args[])
+{
+int num1, num2, temp;
+num1 = 10;
+num2 = 20;
+System.out.println(" num1,num2 = "+num1+" , "+ +num2);
+temp = num1;
+num1 = num2;
+num2 = temp;
+System.out.println("After Swapping : num1,num2 = "+num1+" , "+ +num2);
+}
+}
