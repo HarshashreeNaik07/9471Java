dd136e69f1f374efc161367b7bcce8c2f5734625
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-06 13:37:31 +0530

    Create NestedForLoop.java

---
+++ Anudip Practicals/NestedForLoop.java
@@ -0,0 +1,13 @@
+public class NestedForLoop
+{
+	public static void main(String args[]) 
+ {
+	 for(int i=1; i<=5; i++)
+   {
+	  for(int j=0; j<=3; j++)
+    {
+		System.out.println(i+""+j);
+}
+}
+}
+}
