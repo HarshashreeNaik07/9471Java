38e0cb93dd4fe8452308ce9cb6a98acf1383c16a
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-06 12:36:24 +0530

    Create Animal.java

---
+++ Anudip Practicals/Animal.java
@@ -0,0 +1,18 @@
+//Inheritance example
+
+public class Animal{
+void eat(){
+System.out.println("Eating");
+}}
+class Dog extends Animal{
+void bark(){
+System.out.println("Barking");
+}
+class TestInheritance{
+public static void main(String args[]){
+Dog d = new Dog();
+d.eat();
+d.bark();
+}
+}
+}
