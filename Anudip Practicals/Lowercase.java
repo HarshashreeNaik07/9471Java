04883d1f739b7d173d9daea0ccf3246762976dce
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-07 12:33:23 +0530

    Create Lowercase.java

---
+++ Anudip Practicals/Lowercase.java
@@ -0,0 +1,17 @@
+//String to Lowercase
+
+import java.util.*;
+public class Lowercase {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a String: ");
+		String line = in.nextLine();
+		line = line.toLowerCase();
+		System.out.println(line);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
