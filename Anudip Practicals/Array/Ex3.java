f39232f006e40358fd212ec6ab87432c6769b7f2
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-08 17:00:53 +0530

    Create Ex3.java

---
+++ Anudip Practicals/Array/Ex3.java
@@ -0,0 +1,15 @@
+//Write a Java program to print the following grid.
+
+public class Ex3 {
+	public static void main(String[] args) {
+		int[][]a = new int[10][10];
+		for(int i = 0; i<10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				System.out.printf("-", a[i][j]);
+			}
+			System.out.println();
+    }
+	}
+}
