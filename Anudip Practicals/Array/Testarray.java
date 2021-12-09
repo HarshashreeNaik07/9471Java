56e7ba0e0e6f228b3d3c2ea318904a876d385bce
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-09 10:53:59 +0530

    Create Testarray.java

---
+++ Anudip Practicals/Array/Testarray.java
@@ -0,0 +1,14 @@
+//Array Example
+
+public class Testarray {
+	public static void main(String[] args) {
+		int a[] = new int[5];
+		a[0]=10;
+		a[1]=20;
+		a[2]=70;
+		a[3]=40;
+		a[4]=50;
+		for(int i=0; i<a.length; i++)
+			System.out.println(a[i]);
+	}
+}
