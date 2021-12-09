73a37703c53af6f67c3ca8e01eb7180a709d833a
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-09 10:23:05 +0530

    Create Test4.java

---
+++ Anudip Practicals/icompass/Day1/Test4.java
@@ -0,0 +1,13 @@
+public class Test4 {
+	public static void main(String[] args) {
+		for(int i = 0; i<2; i++)
+		{
+			for (int j=2; j>=0;j--)
+			{
+				if(i==j)
+					break;
+				System.out.println("i=" +i+ "j=" +j);
+			}
+		}
+	}
+}
