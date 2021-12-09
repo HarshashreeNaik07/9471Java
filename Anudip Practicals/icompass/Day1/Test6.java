2d68c61bf20a857f82ddd7d4ba2cd596004a8600
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-09 10:35:18 +0530

    Create Test6.java

---
+++ Anudip Practicals/icompass/Day1/Test6.java
@@ -0,0 +1,8 @@
+public class Test6 {
+	public static void main(String[] args) {
+		int a=5, b=6, c=7;
+		System.out.println("Value is " + b + c);
+		System.out.println(a+b+c);
+		System.out.println("String"+(b+c));
+	}
+}
