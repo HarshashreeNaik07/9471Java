7a23a5cc8529ea16c26a64e5966617b7bff49baa
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-09 22:26:10 +0530

    Create Test12.java

---
+++ Anudip Practicals/icompass/Day4/Test12.java
@@ -0,0 +1,23 @@
+class Employee {
+	String name;
+	int id;
+	public Employee()
+	{
+		this.name = name;
+		this.id = 1;
+	}
+}
+class Manager extends Employee
+{
+String name;
+int id;
+}
+public class Test12 {
+	public static void main(String ars[])
+	{
+		Employee a= new Manager();
+		System.out.println(a);
+	
+	}
+
+}
