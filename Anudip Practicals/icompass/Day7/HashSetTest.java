60284bff23ca1d2be42ff72b4d2b9a2266bff843
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-10 21:11:16 +0530

    Create HashSetTest.java

---
+++ Anudip Practicals/icompass/Day7/HashSetTest.java
@@ -0,0 +1,10 @@
+public class HashSetTest {
+	public static void main(String args[])
+	{
+		Map map = new HashSetTest();
+		map.put(new String("a"),"Audi");
+		map.put(new String("a"), "Ferrari");
+		System.out.println(map);
+	}
+
+}
