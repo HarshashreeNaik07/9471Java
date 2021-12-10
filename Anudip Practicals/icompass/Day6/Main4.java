4b5cb3df3cf7c3c43dd86683446cd5618260e4dd
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2021-12-10 20:41:40 +0530

    Create Main4.java

---
+++ Anudip Practicals/icompass/Day6/Main4.java
@@ -0,0 +1,21 @@
+public class Main4 
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			int a[]= {1,2,3,4};
+			for(int i=1;i<4;i++)
+			{
+				System.out.println("a["+i+"]="+a[i]+"n");
+				
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("error=" +e);
+		}
+	}
+	
+
+}
