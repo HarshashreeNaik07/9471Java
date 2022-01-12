fc14ab22fdcf5ca6d1e92f4df1d1c7d120a5925e
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2022-01-12 21:04:50 +0530

    Create Strings.java

---
+++ Anudip Practicals/Strings/Strings.java
@@ -0,0 +1,15 @@
+public class Strings {
+
+	public static void main(String[] args) {
+
+		String str1 = "Welcome";
+		char[] arr = {'t','o'};
+		String str2 = new String(arr);
+		String str3 = new String("today's lecture ");
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+
+	}
+
+}
