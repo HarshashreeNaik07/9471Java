5fedbe9cac32fb43bad25379c36fb41511bca574
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2022-01-14 00:03:31 +0530

    Create ColorArray.java

---
+++ Anudip Practicals/ArrayAssignment/ColorArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ColorArray {
+
+	public static void main(String[] args) {
+		
+		List<String>  list_Strings = new ArrayList<String>();
+		list_Strings.add("Purple");
+		list_Strings.add("Maroon");
+		list_Strings.add("Black");
+		list_Strings.add("Pink");
+		list_Strings.add("Brown");
+		System.out.println(list_Strings);
+
+	}
+
+}
