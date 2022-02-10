8cd9373d0fac66c72d75cf8dec68d41df02f347c
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2022-02-10 10:13:55 +0530

    Add files via upload

---
+++ Anudip Practicals/JPA_SPRINT_1/JPQLInheritance/src/main/java/com/demo/JPQLInheritance/EmployeePersistence.java
@@ -0,0 +1,36 @@
+package com.demo.JPQLInheritance;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.demo.JPQLInheritance.entity.ActiveEmployee;
+import com.demo.JPQLInheritance.entity.RetiredEmployee;
+
+
+public class EmployeePersistence {  
+    
+    public static void main(String[] args) {  
+    EntityManagerFactory emf=Persistence.createEntityManagerFactory("Employee_details");  
+    EntityManager em=emf.createEntityManager();  
+      
+    em.getTransaction().begin();  
+     
+ActiveEmployee ae1=new ActiveEmployee(101,"Akshay",10000,5);  
+ActiveEmployee ae2=new ActiveEmployee(102,"Chotu",12000,7);  
+  
+RetiredEmployee re1=new RetiredEmployee(103,"Ram",5000,5);  
+RetiredEmployee re2=new RetiredEmployee(104,"Raj",4000,7);  
+  
+    em.persist(ae1);  
+    em.persist(ae2);  
+      
+    em.persist(re1);  
+    em.persist(re2);  
+      
+    em.getTransaction().commit();  
+      
+    em.close();  
+    emf.close();  
+      
+}}
---
+++ Anudip Practicals/JPA_SPRINT_1/JPQLInheritance/src/main/java/com/demo/JPQLInheritance/entity/Employee.java
@@ -0,0 +1,37 @@
+package com.demo.JPQLInheritance.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Employee_details")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE) 
+
+public class Employee {
+	private int s_id;
+	private String s_name;
+	
+	public int getS_id() {
+		return s_id;
+	}
+	public void setS_id(int s_id) {
+		this.s_id = s_id;
+	}
+	public String getS_name() {
+		return s_name;
+	}
+	public void setS_name(String s_name) {
+		this.s_name = s_name;
+	}
+	
+	public Employee() {}
+	
+	public Employee(int s_id, String s_name) {
+		super();
+		this.s_id = s_id;
+		this.s_name = s_name;
+	}
+
+	
+}
