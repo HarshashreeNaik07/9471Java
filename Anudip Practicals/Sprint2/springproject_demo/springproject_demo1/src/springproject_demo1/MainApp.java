38d7c6a25da538687de2341343adf737e5bfe910
HarshashreeNaik07 <95093915+HarshashreeNaik07@users.noreply.github.com>
2022-02-19 11:33:55 +0530

    Add files via upload

---
+++ Anudip Practicals/Sprint2/springproject_demo/springproject_demo1/src/springproject_demo1/MainApp.java
@@ -0,0 +1,17 @@
+package springproject_demo1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+	   public static void main(String[] args) {
+	      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+	      objA.setMessage("I'm object A");
+	      objA.getMessage();
+
+	      HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+	      objB.getMessage();
+	   }
+	}
